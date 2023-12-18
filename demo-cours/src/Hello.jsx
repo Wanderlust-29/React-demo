ac8ae605149100d6ea095a468b5d2dda375c0525
Wanderlust-29 <ryanroudaut8@gmail.com>
2023-12-18 10:49:14 +0000

    fin demo

---
+++ demo-cours/src/Hello.jsx
@@ -0,0 +1,14 @@
+import ShoppingList from "./ShoppingList"
+
+const Hello = (props) => {
+  return (
+    <article className="heading" >
+    <ShoppingList />
+        <h1>Hello</h1>
+        <p>Comment ça va ?</p>
+        
+    </article>
+  )
+}
+
+export default Hello;
---
+++ demo-cours/src/ShoppingList.jsx
@@ -0,0 +1,14 @@
+const ShoppingList = (props) => {
+    const items = ['Pain', 'Beurre', 'Boursin'];
+
+    return (
+      <>
+        <h1>Ma liste de courses</h1>
+        <ul>
+            {items.map((item, i) => <li key={i}>{item}</li>)}
+        </ul>
+      </>
+    )
+}
+
+export default ShoppingList;
